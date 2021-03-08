f21faf0497ed0edadf84706f5fa133d3f2731099
bavario <23457846+bavario@users.noreply.github.com>
2021-03-08 12:17:39 +0100

    Create Aufgabe 4.js

---
+++ tasks/Aufgabe 4.js
@@ -0,0 +1,23 @@
+function likes(names) {
+
+}
+
+// ------------------------------------------------------------------------------------
+
+describe('Aufgabe 4', () => {
+    it('empty', () => {
+        expect(likes([])).toBe('no one likes this');
+    });
+
+    it('single', () => {
+        expect(likes(['Peter'])).toBe('Peter likes this');
+    });
+
+    it('multiple', () => {
+        expect(likes(['Jacob', 'Alex'])).toBe('Jacob and Alex like this');
+    });
+
+    it('> 3', function () {
+        expect(likes(['Alex', 'Jacob', 'Mark', 'Max'])).toBe('Alex, Jacob and 2 others like this');
+    });
+});
