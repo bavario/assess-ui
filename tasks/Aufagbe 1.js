d5c93066182b77610bd3bcd27f5ff7a0db16cb48
bavario <23457846+bavario@users.noreply.github.com>
2021-03-08 12:16:33 +0100

    Create Aufagbe 1.js

---
+++ tasks/Aufagbe 1.js
@@ -0,0 +1,9 @@
+function revert(input) {
+    
+}
+
+// ------------------------------------------------------------------------------------
+
+it('Aufgabe 1', function () {
+    expect(revert('Interhyp')).toBe('pyhretnI');
+});
