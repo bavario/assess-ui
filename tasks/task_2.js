14a19956872106bcb323f07530957963e974fb29
bavario <mario.obendorfer@gmail.com>
2021-05-05 13:43:34 +0000

    new assertion

--- tasks/task_2.js
+++ tasks/task_2.js
@@ -6,11 +6,15 @@ function isPalindrome(input) {
 
 describe('Task 2', () => {
     it('should return true', function () {
-        expect(isPalindrome('Otto')).toBeTruthy();
+        expect(isPalindrome('12221')).toBeTruthy();
     });
 
     it('should return false', function () {
         expect(revert('Interhyp')).toBeFalsy();
+    });
+
+    it('should return true', function () {
+        expect(isPalindrome('Regallager')).toBeTruthy();
     });
 });
 
