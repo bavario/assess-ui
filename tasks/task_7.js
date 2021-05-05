369abd28039dd5a8fda288a4e0dd4a9171da3ac5
bavario <mario.obendorfer@gmail.com>
2021-05-05 13:29:40 +0000

    new test and new task

--- tasks/task_7.js
+++ tasks/task_7.js
@@ -10,4 +10,8 @@ describe('Task 7', () => {
     it("should return keys and values", () => {
         expext(keysAndValues({a: 1, b: 2, c: 3}).toBe([['a', 'b', 'c'], [1, 2, 3]]));
     });
+
+    it("should return keys and values, with keys sorted alphabetically", () => {
+        expext(keysAndValues({b: 2, a: 4, c: 3}).toBe([['a', 'b', 'c'], [4, 2, 3]]));
+    });
 });
